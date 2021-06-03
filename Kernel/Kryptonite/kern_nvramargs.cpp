768f04e02a3e6f10cf51fc74dec5dab329225dd2
Mayank Kumar <mayankk2308@icloud.com>
2021-06-03 13:35:59 -0700

    Process thunderbolt version boot-args

--- Kernel/Kryptonite/kern_nvramargs.cpp
+++ Kernel/Kryptonite/kern_nvramargs.cpp
@@ -18,8 +18,24 @@ void NVRAMArgs::getGPU() {
     SYSLOG(moduleName, "GPU vendor: %s", gpu);
 }
 
+void NVRAMArgs::getTBTVersion() {
+    if (!PE_parse_boot_argn(tbtVersionArg, &tbtVersion, sizeof(tbtVersion))) {
+        SYSLOG(moduleName, "Thunderbolt version not provided.");
+        tbtVersion = 3;
+        return;
+    }
+    
+    if (tbtVersion < 0 | tbtVersion > 4) {
+        SYSLOG(moduleName, "Invalid thunderbolt version provided.");
+        return;
+    }
+    
+    SYSLOG(moduleName, "Provided thunderbolt version: %d", tbtVersion);
+}
+
 void NVRAMArgs::init() {
     getGPU();
+    getTBTVersion();
 }
 
 bool NVRAMArgs::isAMD() {
--- Kernel/Kryptonite/kern_nvramargs.hpp
+++ Kernel/Kryptonite/kern_nvramargs.hpp
@@ -9,15 +9,20 @@
 #define kern_nvramargs_hpp
 
 static char gpu[5];
+static int tbtVersion;
 
 class NVRAMArgs {
 private:
     constexpr static const char* moduleName = "nvram";
+    
     constexpr static const char* gpuArg = "krygpu";
     constexpr static const char* amd = "AMD";
     constexpr static const char* nvda = "NVDA";
     
+    constexpr static const char* tbtVersionArg = "krytbtv";
+    
     void getGPU();
+    void getTBTVersion();
 
 public:
     void init();
